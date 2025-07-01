1be3ff5c8c554288779d3551825c40fd49500614
moh-essamb <moh.essamb@gmail.com>
2025-07-01 10:27:10 +0300

    Commit2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
     // List of products and their quantities
     protected static String[] products = {"Cucumber","5","Carrot","8","Tomato","13", "Mushroom","2"};
+    protected static String parent;
 
     // WebElements
     protected WebElement product, quantityBox;
@@ -32,14 +33,16 @@ public class Main {
 
     // sets quantity and clicks Add to Cart for a single product.
     public void addSingleProductsToCart(String product_Name, String quantity) {
+        parent = "//div[@class='products']/div[@class='product']/h4[contains(text(), '" + product_Name + "')]/parent::div";
         // Locate the quantity input box using product name
-        quantityBox = driver.findElement(By.xpath("//div[@class='products']/div[@class='product']/h4[contains(text(), '" + product_Name + "')]/parent::div/div[@class='stepper-input']/input"));
+        quantityBox = driver.findElement(By.xpath( parent + "/div[@class='stepper-input']/input"));
         deleteTextBox(quantityBox); // Clear the quantity box
         quantityBox.sendKeys(quantity); // Enter new quantity
 
         // Locate the Add to Cart button using product name
-        product = driver.findElement(By.xpath("//div[@class='products']/div[@class='product']/h4[contains(text(), '" + product_Name + "')]/parent::div/div[@class='product-action']/button"));
+        product = driver.findElement(By.xpath(parent + "/div[@class='product-action']/button"));
         product.click();//click the Add to Cart button
+
         //check if the product was added to the cart or not
         if(product.getText().equals("✔ ADDED")){
             System.out.println(quantity+" Nos. of "+product_Name+" was added to the cart ✔");
@@ -49,9 +52,8 @@ public class Main {
         }
     }
 
-    /**
-     * Loops over a list of products and quantities to add each to the cart.
-     */
+
+     //Loops over a list of products and quantities to add each to the cart.
     public void addMultipleProductsToCart(String[] products) {
         for (int i = 0; i < products.length; i+=2) {
             addSingleProductsToCart(products[i],products[i+1]);
